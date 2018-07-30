72e2bc420308d11dc0b4baa937870cc2847213b8
Avigad <avigadm@ibm.com>
2018-07-30 09:27:29 +0300

    comment out cos client

--- src/object_storage_sink.py
+++ src/object_storage_sink.py
@@ -1,6 +1,6 @@
 import logging
-import ibm_boto3
-from ibm_botocore.client import Config
+# import ibm_boto3
+# from ibm_botocore.client import Config
 
 
 class ObjectStorageSink:
@@ -9,16 +9,16 @@ class ObjectStorageSink:
         self.values = []
 
         # ibm cos client initialization
-        cos_endpoint = 'https://s3-api.us-geo.objectstorage.softlayer.net'
-        api_key = 'SlLQ580aUDMQnjlXa_vjaH0998Jz2AMUwQCk_jGiuVP-'
-        service_instance_id = 'crn:v1:bluemix:public:cloud-object-storage:global:a/f730dc759b4c3f320e480cec27def0d9:a88a9266-0500-4f49-a483-f901bba5b1b0::'
-        cos_auth_endpoint = 'https://iam.ng.bluemix.net/oidc/token'
-        self.cos_client = ibm_boto3.resource('s3',
-                                             ibm_api_key_id=api_key,
-                                             ibm_service_instance_id=service_instance_id,
-                                             ibm_auth_endpoint=cos_auth_endpoint,
-                                             config=Config(signature_version='oauth'),
-                                             endpoint_url=cos_endpoint)
+        # cos_endpoint = 'https://s3-api.us-geo.objectstorage.softlayer.net'
+        # api_key = 'SlLQ580aUDMQnjlXa_vjaH0998Jz2AMUwQCk_jGiuVP-'
+        # service_instance_id = 'crn:v1:bluemix:public:cloud-object-storage:global:a/f730dc759b4c3f320e480cec27def0d9:a88a9266-0500-4f49-a483-f901bba5b1b0::'
+        # cos_auth_endpoint = 'https://iam.ng.bluemix.net/oidc/token'
+        # self.cos_client = ibm_boto3.resource('s3',
+        #                                      ibm_api_key_id=api_key,
+        #                                      ibm_service_instance_id=service_instance_id,
+        #                                      ibm_auth_endpoint=cos_auth_endpoint,
+        #                                      config=Config(signature_version='oauth'),
+        #                                      endpoint_url=cos_endpoint)
 
         # self.cos_client.Bucket('data_historian_bucket').create(
         #     CreateBucketConfiguration={
