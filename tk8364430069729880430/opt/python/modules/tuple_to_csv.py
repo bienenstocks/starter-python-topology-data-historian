1e8db7e161d1765ac3bdb130922103cd150a58ed
Avigad <avigadm@ibm.com>
2018-08-05 08:00:26 +0300

    remove mh creds

--- tk8364430069729880430/opt/python/modules/tuple_to_csv.py
+++
@@ -1,9 +0,0 @@
-class TupleToCsv:
-    def __init__(self, attributes_order):
-        self.attributes_order = attributes_order
-
-    def __call__(self, tuple):
-        csv_line = ""
-        for attribute in self.attributes_order:
-            csv_line += str(tuple[attribute]) + ","
-        return csv_line
